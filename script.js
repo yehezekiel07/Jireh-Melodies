88330ab9c5e6559e28dd2f842cf7fc151c354a95
yehezekiel07 <yehezekie.daniel@gmail.com>
2026-03-02 17:50:30 +0530

    Updated Social Media External Links

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 "use strict";
 
 const mainNavList = document.querySelectorAll(".main-nav-list-item");
@@ -20,26 +19,3 @@ faqQuestionBox.forEach((item) => {
     item.classList.add("active");
   });
 });
-=======
-"use strict";
-
-const mainNavList = document.querySelectorAll(".main-nav-list-item");
-mainNavList.forEach((item) => {
-  item.addEventListener("click", () => {
-    for (let i = 0; i < mainNavList.length; i++) {
-      mainNavList[i].classList.remove("active");
-    }
-    item.classList.add("active");
-  });
-});
-
-const faqQuestionBox = document.querySelectorAll(".faq-question-box");
-faqQuestionBox.forEach((item) => {
-  item.addEventListener("click", () => {
-    for (let i = 0; i < faqQuestionBox.length; i++) {
-      faqQuestionBox[i].classList.remove("active");
-    }
-    item.classList.add("active");
-  });
-});
->>>>>>> a850d5403205585d06e15661776e4a5947f0d481
