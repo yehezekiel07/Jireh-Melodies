2b089d06640e9bbd49372b93a76e71abd6577441
yehezekiel07 <yehezekiel.daniel@gmail.com>
2026-03-29 00:56:17 +0530

    Updated few animations

--- public/js/all-courses.js
+++ public/js/all-courses.js
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     data.courses.forEach((course) => {
       const div = document.createElement("div");
-      div.className = "preview-cards reveal-right";
+      div.className = "preview-cards reveal";
 
       div.innerHTML = `
 
